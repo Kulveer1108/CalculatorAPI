d878657c6a6b22342ac325ddfb75624a76360f1d
kulveer1108 <kulveer1108@gmail.com>
2022-03-06 15:07:43 +1100

    Add project files.

---
+++ CalculatorAPI/Controllers/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class Calculator : ControllerBase
+    {
+        [HttpGet(Name = "Calculate")]
+        public double Calculate(double number1, double number2, String Action)
+        {
+            double result = 0;
+            if (Action == "Addition")
+                result = number1 + number2;
+            if (Action == "Multiplication")
+                result = number1 * number2;
+            if (Action == "Subtraction")
+                result = number1 - number2;
+            if (Action == "Division" && number2 != 0)
+                result = number1 / number2;
+            if (Action == "Division" && number2 == 0)
+                result = 0;
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Number : ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number : ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Action : ");
+            string Action = Console.ReadLine();
+
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync("https://localhost:7025/Calculator?number1=" + number1 + "&number2=" + number2 + "&Action=" + Action);
+
+            var responseContent = response.Result;
+
+            Console.WriteLine(responseContent.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
